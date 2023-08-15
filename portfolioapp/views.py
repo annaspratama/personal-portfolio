4a839fd377f91a16faf939ae5218cd034192fe11
Annas Pratama <annaspratama@icloud.com>
2023-08-15 08:28:14 +0700

    Remove unused code

--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -6,14 +6,6 @@ from portfolioapp.models import Portfolio, ProgrammingLanguage
 class PortfolioList(ListView):
     model = Portfolio
 
-    # all_models_dict = {
-    #     'template_name': 'contacts/index.html',
-    #     # 'queryset': Portfolio.objects.all(),
-    #     'extra_context': {
-    #         'programming_language_list' : ProgrammingLanguage.objects.all()
-    #     }
-    # }
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['programming_language_list'] = ProgrammingLanguage.objects.all().order_by('created_date')
