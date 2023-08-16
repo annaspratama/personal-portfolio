0687091ae187a7f8102e45cfbdd789540877db3d
Annas Pratama <annaspratama@icloud.com>
2023-08-16 15:18:50 +0700

    Update static files

--- portfolioproject/settings.py
+++ portfolioproject/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR]
-STATIC_ROOT = STATIC_DIR # Deployment
+STATIC_ROOT = '/staticfiles/' # Deployment
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = MEDIA_DIR
