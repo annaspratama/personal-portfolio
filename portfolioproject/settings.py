7ce0411a9dc204ff62e4fa197cde32a67d5b8d38
Annas Pratama <annaspratama@icloud.com>
2024-02-13 13:58:50 +0700

    Remove unused code

--- portfolioproject/settings.py
+++ portfolioproject/settings.py
@@ -25,14 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-print('debug: ', env('DEBUG'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
-print('secret: ', SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
