3b25cd901c355267e56603e8e09948c0e30c8e61
Annas Pratama <annaspratama@icloud.com>
2024-06-06 13:41:49 +0700

    Add entrypoint to app

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,17 @@ echo "Waiting for PostgreSQL database..."
 while ! nc -z portfolio_db 5432; do
   sleep 1
 done
-echo "The PostgreSQL database is ready!"
+echo "The PostgreSQL database is up."
 
 # Run migrations
+echo "Running collect static files..."
+python manage.py collectstatic --no-input
+echo "Static files has collected."
+
+echo "Running migrations..."
 python manage.py makemigrations
 python manage.py migrate
+echo "Migrations has completed."
 
 # Run the main container command
 exec "$@"
